5fbce3f6a94f19dc77767f93f05b26bac5090f52
Mattias.Poppe <spacid@gmail.com>
2026-01-09 14:05:02 +0100

    Log request

--- app/app.py
+++ app/app.py
@@ -1,15 +1,19 @@
 from litestar import Litestar, get
 from viaa.configuration import ConfigParser
+from viaa.observability import logging
 
 
 def create_app():
-    config = ConfigParser().app_cfg
+    config_parser = ConfigParser()
+    log = logging.get_logger(__name__, config=config_parser)
 
-    name = config["name"]
-    secret = config["secret"]
+    app_cfg = config_parser.app_cfg
+    name = app_cfg["name"]
+    secret = app_cfg["secret"]
 
     @get("/")
     async def hello_world() -> dict[str, str]:
+        log.info(f"{name}: {secret}")
         return {name: secret}
 
     return Litestar(route_handlers=[hello_world])
