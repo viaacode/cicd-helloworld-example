aa7320592a8ec30b76e3a26ca36e141bfbfc2384
Mattias.Poppe <spacid@gmail.com>
2026-02-16 11:08:54 +0100

    Add health and ready routes

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
-from litestar import Litestar, get
+from litestar import Litestar, get, status_codes
 from viaa.configuration import ConfigParser
 from viaa.observability import logging
 
 
-def create_app():
+def create_app() -> Litestar:
     config_parser = ConfigParser()
     log = logging.get_logger(__name__, config=config_parser)
 
@@ -16,4 +16,12 @@ def create_app():
         log.info(f"{name}: {secret}")
         return {name: secret}
 
-    return Litestar(route_handlers=[hello_world])
+    @get("/healthz", status_code=status_codes.HTTP_204_NO_CONTENT)
+    async def health() -> None:
+        return None
+
+    @get("/readyz", status_code=status_codes.HTTP_204_NO_CONTENT)
+    async def ready() -> None:
+        return None
+
+    return Litestar(route_handlers=[hello_world, health, ready])
